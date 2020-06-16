efaffbb9384fd3c08300663a2887452a7ecd3a7f
Samitti <samuelteweldebrhan@gmail.com>
2020-06-16 09:34:59 +0300

    add post helper methods

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,22 @@
 module PostsHelper
+  def loged_user(post)
+    if user_signed_in?
+      post.user.name
+    else
+      "John Doe:"
+    end
+  end
+
+  def sign_in_user
+    if !user_signed_in?
+      "Sign Up or Sign In to see who posted!!!"    
+    end
+  end
+
+  def is_current_user(post)
+    if current_user == post.user
+      link_to 'Edit', edit_post_path(post) 
+    end
+  end
+
 end
